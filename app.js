7090c8db6c7df1f5497c67ffce212e350e1bd0d2
Vlad Sovetov <vsovetov@funambol.com>
2019-10-20 17:06:10 +0300

    chore: Added headers to avoid CORS during development

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ if (process.env.NODE_ENV !== 'test') {
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     next();
 });
 app.use(bodyParser.json());
